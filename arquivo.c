dbb67dfb108bab1191adbedb8d59a320d19c16d4
Jeferson Ferreira <jeferson.oferreira@etec.sp.gov.br>
2024-05-03 14:37:23 -0300

    inclusao do menu de opcoes

--- arquivo.c
+++ arquivo.c
@@ -1,2 +1,21 @@
 #include<stdlib.h>
+#include<stdio.h>
 
+void main(){
+
+	float numero1; 
+	float numero2;
+	float resultado;
+	int opcao;
+
+printf("Calculadora\n");
+printf("Escolha uma das seguinte operações:\n\n");
+printf("\t1- Soma\n");
+printf("\t2- Subtracao\n");
+printf("\t3- Multiplicacao\n");
+printf("\t4- Divisao\n");
+
+	
+
+
+}
